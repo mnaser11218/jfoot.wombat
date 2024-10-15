2a85c2b092f0f2b4be45b96b4870ead876b4347d
Mohammed N <mnaser11218@gmail.com>
2024-10-15 19:07:45 -0400

    lab completed

--- src/main/java/com/github/curriculeon/jfoot/Wombat.java
+++ src/main/java/com/github/curriculeon/jfoot/Wombat.java
@@ -12,18 +12,32 @@ import static com.github.curriculeon.jfoot.Direction.*;
  * @version 2.0
  */
 public class Wombat extends Herbivore {
+ public boolean isEven =true;
     public Wombat() {
         setImage("wombat.png");
     }
 
     public void act() {
+       // boolean isEven = true;
         if (this.foundLeaf()) {
             this.eatLeaf();
         } else if (this.canMove()) {
             this.move();
-        } else {
-            this.turnLeft();
         }
+        else if(!this.canMove() && !isEven){
+            this.turnRight();
+            this.move();
+            this.turnRight();
+            this.isEven = true;
+        }
+        else {
+           this.turnLeft();
+           this.move();
+           this.turnLeft();
+            this.isEven = false;
+        }
+
+
     }
 
     public void turnLeft() {
@@ -35,6 +49,18 @@ public class Wombat extends Herbivore {
             this.setDirection(WEST);
         } else {
             this.setDirection(EAST);
+        }
+    }
+
+    public void turnRight() {
+        if (this.getDirection() == NORTH) {
+            this.setDirection(EAST);
+        } else if (this.getDirection() == SOUTH) {
+            this.setDirection(WEST);
+        } else if (this.getDirection() == WEST) {
+            this.setDirection(NORTH);
+        } else {
+            this.setDirection(SOUTH);
         }
     }
 }
